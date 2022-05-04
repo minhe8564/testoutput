5566525bd1e400b2d94132e3cce81309e1726748
minhe8564 <91974925+minhe8564@users.noreply.github.com>
2022-05-04 10:49:11 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True,
+  name = 'testoutput1205',
+  version = '0.0.1',
+  description = 'oss-dev my testoutput1205',
+  author = 'minhe8564',
+  author_email = 'minhe8564@gmail.com',
+  url = "https://github.com/minhe8564/testoutput",
+  install_requires=[ 'pytest' ],
+  long_description = 'oss-dev python module',
+  long_description_content_type = 'text/markdown',
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "Operationg System :: OS Independent",
+  ],
+)
